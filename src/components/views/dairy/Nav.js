f98e6662cd72e8bc957eb43eb754f81a08a75b8b
Mohammad Hamza <zam.tech.info@gmail.com>
2022-05-08 18:29:02 +0500

    design navbar

--- src/components/views/dairy/Nav.js
+++ src/components/views/dairy/Nav.js
@@ -5,12 +5,11 @@ const Nav = (props) => {
        
             <nav className="sub-nav">
                 <ul className="sub-links">
-                    <NavLink to="Namaz" className="sub-nav-links" ><li >Namaz</li></NavLink>
-                    <NavLink to="Quran" className="sub-nav-links"><li >Quran</li></NavLink>
-                    <NavLink to="Hadees" className="sub-nav-links"><li >Hadees</li></NavLink>
-                    <NavLink to="letrature" className="sub-nav-links"><li >Letrature</li></NavLink>
-                    <NavLink to="meetings" className="sub-nav-links"><li >Meetings</li></NavLink>
-
+                    <NavLink to="Namaz" className="sub-nav-links" ><li >Namaz</li> <div class="bottom"></div> </NavLink>
+                    <NavLink to="Quran" className="sub-nav-links"><li >Quran</li> <div class="bottom"></div> </NavLink>
+                    <NavLink to="Hadees" className="sub-nav-links"><li >Hadees</li> <div class="bottom"></div> </NavLink>
+                    <NavLink to="letrature" className="sub-nav-links"><li >Letrature</li> <div class="bottom"></div> </NavLink>
+                    <NavLink to="meetings" className="sub-nav-links"><li >Meetings</li> <div class="bottom"></div></NavLink>
                 </ul>
             </nav>
       
